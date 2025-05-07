31dea87b29827c5b966b3f7e6a12d74ed252de60
CimanesDev <jmcimanes@up.edu.ph>
2025-05-08 02:12:43 +0800

    build fixes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfoliov2/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  }
 })
